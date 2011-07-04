fffcbc65efecc16b8661034b2baff1842ebebfd1
Cian Wilson <cwilson@ldeo.columbia.edu>
2011-07-04 00:54:25 -0400

    The start of a new script to write the namespace header file.  This has turned out to be much more complicated than I expected so will ponder ways to simplify tomorrow.

---
+++ buckettools/scripts/write_systems_wrapper_spud.py
@@ -0,0 +1,226 @@
+
+from optparse import OptionParser
+import sys
+import libspud
+import ufltools.spud
+
+# Let's start by parsing the options
+optparser=OptionParser(usage='usage: %prog <options-file>',
+                       add_help_option=True,
+                       description="""This script takes a buckettools options file and writes""" +
+                       """ufl files based on the nonlinear_solver and diagnostic functional options.""")
+
+(options, argv) = optparser.parse_args()
+
+if len(argv)<1:
+    optparser.print_help()
+    sys.exit(1)
+
+# the options file name
+options_filename  = argv[0]
+
+# load the options tree
+libspud.load_options(options_filename)
+
+cpp       = []
+cpp.append("#ifndef __SYSTEMS_WRAPPER_H\n")
+cpp.append("#define __SYSTEMS_WRAPPER_H\n")
+cpp.append("\n")
+cpp.append("#include \"Bucket.h\"\n")
+cpp.append("#include <dolfin.h>\n")
+cpp.append("\n")
+
+include_cpp = []
+
+functionspace_cpp         = []
+functionspace_cpp.append("  FunctionSpace_ptr fetch_functionspace(std::string systemname, std::string functionname, Mesh_ptr mesh)\n")
+functionspace_cpp.append("  {\n")
+functionspace_cpp.append("  \n")
+functionspace_cpp.append("    switch(systemname)\n")
+functionspace_cpp.append("    {\n")
+functionspace_cpp.append("    \n")
+
+functional_cpp            = []
+functional_cpp.append("  Form_ptr fetch_functional(std::string systemname, std::string functionname, std::string functionalname, Mesh_ptr mesh)\n")
+functional_cpp.append("  {\n")
+functional_cpp.append("    switch(systemname)\n")
+functional_cpp.append("    {\n")
+
+picard_bilinearform_cpp   = []
+picard_bilinearform_cpp.append("  Form_ptr fetch_picard_bilinearform(std::string systemname, std::string functionname, std::string solvername, FunctionSpace_ptr functionspace)\n")
+picard_bilinearform_cpp.append("  {\n")
+picard_bilinearform_cpp.append("  \n")
+picard_bilinearform_cpp.append("    switch(systemname)\n")
+picard_bilinearform_cpp.append("    {\n")
+picard_bilinearform_cpp.append("    \n")
+
+picard_bilinearpcform_cpp = []
+picard_bilinearpcform_cpp.append("  Form_ptr fetch_picard_bilinearpcform(std::string systemname, std::string functionname, std::string solvername, FunctionSpace_ptr functionspace)\n")
+picard_bilinearpcform_cpp.append("  {\n")
+picard_bilinearpcform_cpp.append("  \n")
+picard_bilinearpcform_cpp.append("    switch(systemname)\n")
+picard_bilinearpcform_cpp.append("    {\n")
+picard_bilinearpcform_cpp.append("    \n")
+
+picard_linearform_cpp     = []
+picard_linearform_cpp.append("  Form_ptr fetch_picard_linearform(std::string systemname, std::string functionname, std::string solvername, FunctionSpace_ptr functionspace)\n")
+picard_linearform_cpp.append("  {\n")
+picard_linearform_cpp.append("  \n")
+picard_linearform_cpp.append("    switch(systemname)\n")
+picard_linearform_cpp.append("    {\n")
+picard_linearform_cpp.append("    \n")
+
+picard_residualform_cpp   = []
+picard_residualform_cpp.append("  Form_ptr fetch_picard_residualform(std::string systemname, std::string functionname, std::string solvername, FunctionSpace_ptr functionspace)\n")
+picard_residualform_cpp.append("  {\n")
+picard_residualform_cpp.append("  \n")
+picard_residualform_cpp.append("    switch(systemname)\n")
+picard_residualform_cpp.append("    {\n")
+picard_residualform_cpp.append("    \n")
+
+snes_residualform_cpp     = []
+snes_residualform_cpp.append("  Form_ptr fetch_snes_residualform(std::string systemname, std::string functionname, std::string solvername, FunctionSpace_ptr functionspace)\n")
+snes_residualform_cpp.append("  {\n")
+snes_residualform_cpp.append("  \n")
+snes_residualform_cpp.append("    switch(systemname)\n")
+snes_residualform_cpp.append("    {\n")
+snes_residualform_cpp.append("    \n")
+
+snes_jacobianform_cpp     = []
+snes_jacobianform_cpp.append("  Form_ptr fetch_snes_jacobianform(std::string systemname, std::string functionname, std::string solvername, FunctionSpace_ptr functionspace)\n")
+snes_jacobianform_cpp.append("  {\n")
+snes_jacobianform_cpp.append("  \n")
+snes_jacobianform_cpp.append("    switch(systemname)\n")
+snes_jacobianform_cpp.append("    {\n")
+snes_jacobianform_cpp.append("    \n")
+
+snes_jacobianpcform_cpp   = []
+snes_jacobianpcform_cpp.append("  Form_ptr fetch_snes_jacobianpcform(std::string systemname, std::string functionname, std::string solvername, FunctionSpace_ptr functionspace)\n")
+snes_jacobianpcform_cpp.append("  {\n")
+snes_jacobianpcform_cpp.append("  \n")
+snes_jacobianpcform_cpp.append("    switch(systemname)\n")
+snes_jacobianpcform_cpp.append("    {\n")
+snes_jacobianpcform_cpp.append("    \n")
+
+# loop over the systems in the options tree
+for i in range(libspud.option_count("/system")):
+  system_optionpath = "/system["+`i`+"]"
+  system = ufltools.spud.SpudSystem()
+  # get all the information about this system from the options dictionary
+  system.fill(system_optionpath)
+
+  functional_cpp.append("      case \""+system.name+"\":\n")
+  functional_cpp.append("        switch(functionname)\n")
+  functional_cpp.append("        {\n")
+
+  for j in range(libspud.option_count(system_optionpath+"/field")):
+    field_optionpath = system_optionpath+"/field["+`j`+"]"
+    field = ufltools.spud.SpudFunction()
+    # get all the information about this field from the options dictionary
+    field.fill(field_optionpath, system)
+    # let the system know about this field
+    system.fields.append(field)
+
+    functional_cpp.append("          case \""+field.name+"\":\n")
+    functional_cpp.append("            switch(functionalname)\n")
+    functional_cpp.append("            {\n")
+
+    for k in range(libspud.option_count(field_optionpath+"/type/output/include_in_diagnostics/functional")):
+      functional_optionpath = field_optionpath+"/type/output/include_in_diagnostics/functional["+`k`+"]"
+      functional = ufltools.spud.SpudFunctional()
+      # get all the information about this functional from the options dictionary
+      functional.fill(functional_optionpath, field)
+      # let the field know about this functional
+      field.functionals.append(functional)
+      
+      include_cpp.append("#include \""+system.name+field.name+functional.name+".h\"\n")
+      
+      functional_cpp.append("              case \""+functional.name+"\":\n")
+      functional_cpp.append("                Form_ptr functional(new "+system.name+field.name+functional.name+"::Form_0(*functionspace));\n")
+      functional_cpp.append("                break;\n")
+      
+      # done with this functional
+      del functional
+
+    functional_cpp.append("              default:\n")
+    functional_cpp.append("                dolfin::error(\"Unknown functionalname in fetch_functional\")\n")
+    functional_cpp.append("            }\n")
+    functional_cpp.append("            break;\n")
+
+    # remove the local copy of this field
+    del field
+
+  for j in range(libspud.option_count(system_optionpath+"/coefficient")):
+    coeff_optionpath = system_optionpath+"/coefficient["+`j`+"]"
+    coeff = ufltools.spud.SpudFunction()
+    # get all the information about this coefficient from the options dictionary
+    coeff.fill(coeff_optionpath, system)
+    # let the system know about this coefficient
+    system.coeffs.append(coeff)
+
+    functional_cpp.append("          case \""+coeff.name+"\":\n")
+    functional_cpp.append("            switch(functionalname)\n")
+    functional_cpp.append("            {\n")
+
+    for k in range(libspud.option_count(coeff_optionpath+"/type/output/include_in_diagnostics/functional")):
+      # aliased coefficients don't get in here because we're not using the aliased optionpath in the line above
+      functional_optionpath = coeff_optionpath+"/type/output/include_in_diagnostics/functional["+`k`+"]"
+      functional = ufltools.spud.SpudFunctional()
+      # get all the information about this functional from the options dictionary
+      functional.fill(functional_optionpath, coeff)
+      # let the coefficient know about this functional
+      coeff.functionals.append(functional)
+      
+      include_cpp.append("#include \""+system.name+coeff.name+functional.name+".h\"\n")
+      
+      functional_cpp.append("              case \""+functional.name+"\":\n")
+      functional_cpp.append("                Form_ptr functional(new "+system.name+coeff.name+functional.name+"::Form_0(*functionspace));\n")
+      functional_cpp.append("                break;\n")
+      
+      # done with this functional
+      del functional
+
+    functional_cpp.append("              default:\n")
+    functional_cpp.append("                dolfin::error(\"Unknown functionalname in fetch_functional\")\n")
+    functional_cpp.append("            }\n")
+    functional_cpp.append("            break;\n")
+
+    # remove the local copy of this coefficient
+    del coeff
+  
+  functional_cpp.append("          default:\n")
+  functional_cpp.append("            dolfin::error(\"Unknown functionname in fetch_functional\")\n")
+  functional_cpp.append("        }\n")
+  functional_cpp.append("        break;\n")
+
+  for j in range(libspud.option_count(system_optionpath+"/nonlinear_solver")):
+    solver_optionpath = system_optionpath+"/nonlinear_solver["+`j`+"]"
+    solver = ufltools.spud.SpudSolver()
+    # get all the information about this nonlinear solver from the options dictionary
+    solver.fill(solver_optionpath, system)
+    # done with this nonlinear solver
+    del solver
+
+  # done with this system
+  del system
+
+functional_cpp.append("      default:\n")
+functional_cpp.append("        dolfin::error(\"Unknown systemname in fetch_functional\")\n")
+functional_cpp.append("    }\n")
+functional_cpp.append("  }\n")
+
+cpp += include_cpp
+cpp.append("\n")
+cpp.append("namespace buckettools\n")
+cpp.append("{\n")
+cpp += functional_cpp
+cpp.append("}\n")
+cpp.append("\n")
+cpp.append("#endif\n")
+
+filehandle = file("SystemsWrapper.h", 'w')
+filehandle.writelines(cpp)
+filehandle.close()
+
+# and we're done
+
