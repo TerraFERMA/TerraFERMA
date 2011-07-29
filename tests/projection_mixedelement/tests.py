7d2ab2f319da4b665cf811bb4775bb3510cc061d
Cian Wilson <cwilson@ldeo.columbia.edu>
2011-07-29 01:03:44 -0400

    Reording CMakeLists to make verbosity work.  Adding rudimentary testing using py.test - like this but we need a much improved way of using it as it's FIDDLY at the moment.

---
+++ tests/projection_mixedelement/tests.py
@@ -0,0 +1,102 @@
+
+from buckettools.statfile import parser
+import numpy
+
+stat = parser("projection.stat")
+
+def test_picard_field1_min():
+  val = stat["PicardProjection"]["Field1"]["min"][-1]
+  assert val == 1000.0
+
+def test_picard_field1_max():
+  val = stat["PicardProjection"]["Field1"]["max"][-1]
+  assert val == 1000.0
+
+def test_picard_field1_int():
+  val = stat["PicardProjection"]["Field1"]["Integral"][-1]
+  assert val == 1000.0
+
+def test_picard_source1_int():
+  val = stat["PicardProjection"]["Source1"]["Integral"][-1]
+  assert val == 1000.0
+
+def test_picard_field2_min_0():
+  val = stat["PicardProjection"]["Field2"]["max"][0][-1]
+  assert val == 2000.0
+
+def test_picard_field2_max_0():
+  val = stat["PicardProjection"]["Field2"]["min"][0][-1]
+  assert val == 2000.0
+
+def test_picard_field2_int_0():
+  val = stat["PicardProjection"]["Field2"]["Integral0"][-1]
+  assert val == 2000.0
+
+def test_picard_source2_int_0():
+  val = stat["PicardProjection"]["Source2"]["Integral0"][-1]
+  assert val == 2000.0
+
+def test_picard_field2_min_1():
+  val = stat["PicardProjection"]["Field2"]["max"][1][-1]
+  assert val == 3000.0
+
+def test_picard_field2_max_1():
+  val = stat["PicardProjection"]["Field2"]["min"][1][-1]
+  assert val == 3000.0
+
+def test_picard_field2_int_1():
+  val = stat["PicardProjection"]["Field2"]["Integral1"][-1]
+  assert val == 3000.0
+
+def test_picard_source2_int_1():
+  val = stat["PicardProjection"]["Source2"]["Integral1"][-1]
+  assert val == 3000.0
+
+def test_snes_field1_min():
+  val = stat["SNESProjection"]["Field1"]["min"][-1]
+  assert val == 1000.0
+
+def test_snes_field1_max():
+  val = stat["SNESProjection"]["Field1"]["max"][-1]
+  assert val == 1000.0
+
+def test_snes_field1_int():
+  val = stat["SNESProjection"]["Field1"]["Integral"][-1]
+  assert val == 1000.0
+
+def test_snes_source1_int():
+  val = stat["SNESProjection"]["Source1"]["Integral"][-1]
+  assert val == 1000.0
+
+def test_snes_field2_min_0():
+  val = stat["SNESProjection"]["Field2"]["max"][0][-1]
+  assert val == 2000.0
+
+def test_snes_field2_max_0():
+  val = stat["SNESProjection"]["Field2"]["min"][0][-1]
+  assert val == 2000.0
+
+def test_snes_field2_int_0():
+  val = stat["SNESProjection"]["Field2"]["Integral0"][-1]
+  assert val == 2000.0
+
+def test_snes_source2_int_0():
+  val = stat["SNESProjection"]["Source2"]["Integral0"][-1]
+  assert val == 2000.0
+
+def test_snes_field2_min_1():
+  val = stat["SNESProjection"]["Field2"]["max"][1][-1]
+  assert val == 3000.0
+
+def test_snes_field2_max_1():
+  val = stat["SNESProjection"]["Field2"]["min"][1][-1]
+  assert val == 3000.0
+
+def test_snes_field2_int_1():
+  val = stat["SNESProjection"]["Field2"]["Integral1"][-1]
+  assert val == 3000.0
+
+def test_snes_source2_int_1():
+  val = stat["SNESProjection"]["Source2"]["Integral1"][-1]
+  assert val == 3000.0
+
