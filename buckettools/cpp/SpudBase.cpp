e23bcd926c3f14040f6a441f6755be60634187de
Cian Wilson <cwilson@ldeo.columbia.edu>
2011-08-27 12:13:03 +0100

    comment spud base

--- buckettools/cpp/SpudBase.cpp
+++ buckettools/cpp/SpudBase.cpp
@@ -7,33 +7,39 @@
 
 using namespace buckettools;
 
-// initialize an expression using spud
-Expression_ptr buckettools::initialize_expression(const std::string &optionpath, const int &size, const std::vector<int> &shape)
+//*******************************************************************|************************************************************//
+// initialize an expression from an optionpath assuming the buckettools schema
+//*******************************************************************|************************************************************//
+Expression_ptr buckettools::initialize_expression(const std::string 
+                                                        &optionpath, 
+                                                  const int &size, 
+                                                  const std::vector<int> 
+                                                        &shape)
 {
-  Spud::OptionError serr;
-  // declare the pointer we'll be returning
-  Expression_ptr expression;
+  Spud::OptionError serr;                                            // spud option error
+  Expression_ptr expression;                                         // declare the pointer that will be returned
   
-  std::stringstream constbuffer, pybuffer;
-  constbuffer.str(""); constbuffer << optionpath << "/constant";
-  pybuffer.str(""); pybuffer << optionpath << "/python";
+  std::stringstream constbuffer, pybuffer;                           // some string assembly streams
+  constbuffer.str(""); constbuffer << optionpath << "/constant";     // for a constant
+  pybuffer.str(""); pybuffer << optionpath << "/python";             // for a python function
   
-  // Are we constant or python (or something else we don't know about yet)?
-  if (Spud::have_option(constbuffer.str()))
+  if (Spud::have_option(constbuffer.str()))                          // constant requested?
   {
-    // constant
     int rank;
-    serr = Spud::get_option_rank(constbuffer.str(), rank); spud_err(constbuffer.str(), serr);
-    if(rank==0)
+    serr = Spud::get_option_rank(constbuffer.str(), rank);           // find out the rank
+    spud_err(constbuffer.str(), serr);
+    if(rank==0)                                                      // scalar
     {
       double value;
-      serr = Spud::get_option(constbuffer.str(), value); spud_err(constbuffer.str(), serr);
+      serr = Spud::get_option(constbuffer.str(), value); 
+      spud_err(constbuffer.str(), serr);
       expression.reset(new dolfin::Constant(value));
     }
-    else if (rank==1)
+    else if (rank==1)                                                // vector
     {
       std::vector<double> values;
-      serr = Spud::get_option(constbuffer.str(), values); spud_err(constbuffer.str(), serr);
+      serr = Spud::get_option(constbuffer.str(), values); 
+      spud_err(constbuffer.str(), serr);
       assert(values.size()==size);
       expression.reset(new dolfin::Constant(values));
     }
@@ -51,31 +57,33 @@ Expression_ptr buckettools::initialize_expression(const std::string &optionpath,
 //    {
 //      dolfin::error("Unknown rank in init_exp_");
 //    }
-    else
+    else                                                             // unable to deal with this rank
     {
       dolfin::error("Don't deal with rank > 1 yet.");
     }
   } 
-  else if (Spud::have_option(pybuffer.str()))
+  else if (Spud::have_option(pybuffer.str()))                        // python requested
   {
-    // python
-    std::string pyfunction;
-    serr = Spud::get_option(pybuffer.str(), pyfunction); spud_err(pybuffer.str(), serr);
+    std::string pyfunction;                                          // the python function string
+    serr = Spud::get_option(pybuffer.str(), pyfunction); 
+    spud_err(pybuffer.str(), serr);
     
-    // rank of a python function isn't in the default spud base language
-    // so have added it... but it comes out as a string of course!
+                                                                     // rank of a python function isn't in the default spud base
+                                                                     // language so have added it... but it comes out as a string 
+                                                                     // of course!
     std::stringstream buffer;
-    std::string rankstring; // bit of a hack
+    std::string rankstring;                                          // bit of a hack
     buffer.str(""); buffer << pybuffer.str() << "/rank";
-    serr = Spud::get_option(buffer.str(), rankstring); spud_err(buffer.str(), serr);
+    serr = Spud::get_option(buffer.str(), rankstring); 
+    spud_err(buffer.str(), serr);
     
     int rank;
     rank = atoi(rankstring.c_str());
-    if(rank==0)
+    if(rank==0)                                                      // scalar
     {
       expression.reset(new buckettools::PythonExpression(pyfunction));
     }
-    else if (rank==1)
+    else if (rank==1)                                                // vector
     {
       expression.reset(new buckettools::PythonExpression(size, pyfunction));
     }
@@ -84,47 +92,59 @@ Expression_ptr buckettools::initialize_expression(const std::string &optionpath,
       dolfin::error("Don't deal with rank > 1 yet.");
     }
   }
-  else
+  else                                                               // unknown expression type
   {
     dolfin::error("Unknown way of specifying expression.");
   }
   
-  return expression;
+  return expression;                                                 // return the initialized expression
   
 }
 
 
-// Handle option errors through dolfin::error
+//*******************************************************************|************************************************************//
+// return dolfin errors after failures in spud
+//*******************************************************************|************************************************************//
 void buckettools::spud_err(const std::string &optionpath, 
                            const Spud::OptionError &error)
 {
   switch (error)
   {
-    case Spud::SPUD_NO_ERROR:
+    case Spud::SPUD_NO_ERROR:                                        // no error
       break;
-    case Spud::SPUD_KEY_ERROR:
-      dolfin::error("Option key error. Key is: %s", optionpath.c_str());
+    case Spud::SPUD_KEY_ERROR:                                       // key error, e.g. unknown key
+      dolfin::error("Option key error. Key is: %s", 
+                                                optionpath.c_str());
       break;
-    case Spud::SPUD_TYPE_ERROR:
-      dolfin::error("Option type error. Key is: ", optionpath.c_str());
+    case Spud::SPUD_TYPE_ERROR:                                      // type error, e.g. integer not float
+      dolfin::error("Option type error. Key is: %s", 
+                                                optionpath.c_str());
       break;
-    case Spud::SPUD_RANK_ERROR:
-      dolfin::error("Option rank error. Key is: ", optionpath.c_str());
+    case Spud::SPUD_RANK_ERROR:                                      // rank error, e.g. wrong rank passed in
+      dolfin::error("Option rank error. Key is: %s", 
+                                                optionpath.c_str());
       break;
-    case Spud::SPUD_SHAPE_ERROR:
-      dolfin::error("Option shape error. Key is: ", optionpath.c_str());
+    case Spud::SPUD_SHAPE_ERROR:                                     // shape error, e.g. wrong shape
+      dolfin::error("Option shape error. Key is: %s", 
+                                                optionpath.c_str());
       break;
-    case Spud::SPUD_FILE_ERROR:
-      dolfin::error("Option file error. Filename is: ", optionpath.c_str());
+    case Spud::SPUD_FILE_ERROR:                                      // file error
+      dolfin::error("Option file error. Filename is: %s", 
+                                                optionpath.c_str());
       break;
-    case Spud::SPUD_NEW_KEY_WARNING:
-      dolfin::error("Option warning. Key is not in the options tree: ", optionpath.c_str());
+    case Spud::SPUD_NEW_KEY_WARNING:                                 // adding a new key
+      dolfin::error(                                                 // FIXME: not an error
+                "Option warning. Key is not in the options tree: %s",
+                                                optionpath.c_str());
       break;
-    case Spud::SPUD_ATTR_SET_FAILED_WARNING:
-      dolfin::error("Option warning. Option cannot be set as an attribute. Key is ", optionpath.c_str());
+    case Spud::SPUD_ATTR_SET_FAILED_WARNING:                         // attribute error
+      dolfin::error(                                                 // FIXME: not an error
+      "Option warning. Option cannot be set as an attribute. Key is %s",
+                                                optionpath.c_str());
       break;
-    default:
-      dolfin::error("Unknown option error. Key is: ", optionpath.c_str());
+    default:                                                         // unknown error
+      dolfin::error("Unknown option error. Key is: ", 
+                                                optionpath.c_str());
   }
 
 }
