e6dd7f0e23e0981c6959cc7ce3f586b397455c7e
Cian Wilson <cwilson@ldeo.columbia.edu>
2011-09-12 15:28:37 -0400

    Lots of work on timestepping... not working but committing to keep track of preconference changes versus those that are postconference.

--- cpp/main.cpp
+++ cpp/main.cpp
@@ -9,30 +9,17 @@ int main(int argc, char* argv[])
   dolfin::set_log_active(true);
   dolfin::set_log_level(dolfin::DBG);
 
-  std::stringstream buffer;
-  Spud::OptionError serr;
-
   const std::string options_filename = argv[1];
   Spud::load_options(options_filename);
 
-  std::string basename;
-  buffer.str(""); buffer << "/io/output_base_name";
-  serr = Spud::get_option(buffer.str(), basename); buckettools::spud_err(buffer.str(), serr);
-
-  buckettools::SpudBucket bucket(basename, "");
+  buckettools::SpudBucket bucket;
 
   bucket.fill();
   bucket.output();
 
-  buckettools::DiagnosticsFile diagfile(basename+".stat");
-  diagfile.write_header(bucket, false);
-  diagfile.write_data(bucket);
-
   bucket.run();
 
   bucket.output();
-
-  diagfile.write_data(bucket);
 
   std::cout << bucket.str();
 
