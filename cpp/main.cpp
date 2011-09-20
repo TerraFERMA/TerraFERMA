51bee63c225fdf0a6292658359cea02edf37f1e0
Cian Wilson <cwilson@ldeo.columbia.edu>
2011-09-19 22:47:31 -0400

    Signal handling.  Solver failures are now caught and throw a sig int.

--- cpp/main.cpp
+++ cpp/main.cpp
@@ -4,15 +4,11 @@
 
 int main(int argc, char* argv[])
 {
-  buckettools::parse_arguments(argc,argv);
+  buckettools::init(argc, argv);
 
   buckettools::SpudBucket bucket;
-
   bucket.fill();
-
   bucket.run();
-
-  std::cout << bucket.str();
 
   return 0;
   
