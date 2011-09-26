87b5fbe8eb5824f106187cbae1e88b39e2a3ec77
Cian Wilson <cwilson@ldeo.columbia.edu>
2011-09-25 23:16:22 -0400

    Adding the time dependent rbconvection, with linear solvers, to the testing suite.  Need a better testharness.

--- tests/rbconvection1a/tests.py
+++ tests/rbconvection1a/tests.py
@@ -5,9 +5,17 @@ from math import sqrt
 
 stat = parser("rbconvection.stat")
 
+def test_timestepcount():
+  val = stat["timestep"]["value"][-1]
+  assert abs(val - 66) < 1
+
+def test_elapsedtime():
+  val = stat["ElapsedTime"]["value"][-1]
+  assert abs(val - 3.3e3) < 5.e1
+
 def test_v_rms():
-  val = sqrt(stat["Stokes"]["Velocity"]["L2Norm"][-1])
-  assert abs(val - 42.865) < 0.01
+  val = sqrt(stat["Stokes"]["Velocity"]["L2NormSquared"][-1])
+  assert abs(val - 42.865e-4) < 0.01
 
 def test_nu():
   val = -1.0*(stat["Stokes"]["Temperature"]["TopSurfaceIntegral"][-1])
