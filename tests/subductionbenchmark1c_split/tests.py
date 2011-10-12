5a24215cf010de803e1ecaf39d6ef25c2f8bb756
Cian Wilson <cwilson@ldeo.columbia.edu>
2011-10-12 02:32:28 -0400

    A split version of the 1a subduction benchmark.  Petsc spits out warnings about zero diagonals, not sure where this is coming from as ident_zeros is turned on and appears to be working.  Does not affect benchmark results.

---
+++ tests/subductionbenchmark1c_split/tests.py
@@ -0,0 +1,19 @@
+
+from buckettools.statfile import parser
+import numpy
+from math import sqrt
+
+det = parser("subduction.det")
+
+def test_T_11_11():
+  val = det["Temperature"]["Temperature"]["SlabPoint"][0,-1]-273.
+  assert abs(val - 388.5) < 1.0
+
+def test_T_slab():
+  val = sqrt(sum((det["Temperature"]["Temperature"]["Slab"][:,-1]-273.)**2)/36.)
+  assert abs(val - 504.0) < 1.0
+
+def test_T_wedge():
+  val = sqrt(sum((det["Temperature"]["Temperature"]["Wedge"][:,-1]-273.)**2)/78.)
+  assert abs(val - 853.5) < 1.0
+
