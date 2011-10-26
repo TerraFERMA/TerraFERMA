554c8ef053076bf34937eb5e0af10423e82f9187
Cian Wilson <cwilson@ldeo.columbia.edu>
2011-10-26 01:10:00 -0400

    Beefing up the projection_cpp test case to represent the new c++ expression functionality of buckettools.  It now has time dependent functions, dependencies between functions, depdencies between expressions, depdencies between systems and depdendencies between fields.

--- tests/projection_cpp/tests.py
+++ tests/projection_cpp/tests.py
@@ -196,3 +196,51 @@ def test_snes_pysource2_oldint_1():
   val = stat["SNESProjection"]["PySource2"]["OldIntegral1"]
   assert numpy.all(val == [0.0, 0.0, 300.0, 600.0, 900.0, 1200.0, 1500.0, 1800.0, 2100.0, 2400.0, 2700.0])
 
+def test_picard_fieldeval1_min():
+  val = stat["PicardProjection"]["FieldEval1"]["min"]
+  assert numpy.all(val == [0.0, 100.0, 200.0, 300.0, 400.0, 500.0, 600.0, 700.0, 800.0, 900.0, 1000.0])
+
+def test_picard_fieldeval1_max():
+  val = stat["PicardProjection"]["FieldEval1"]["max"]
+  assert numpy.all(val == [0.0, 100.0, 200.0, 300.0, 400.0, 500.0, 600.0, 700.0, 800.0, 900.0, 1000.0])
+
+def test_picard_fieldeval1_int():
+  val = stat["PicardProjection"]["FieldEval1"]["Integral"]
+  assert numpy.all(val == [0.0, 100.0, 200.0, 300.0, 400.0, 500.0, 600.0, 700.0, 800.0, 900.0, 1000.0])
+
+def test_picard_fieldeval1_oldint():
+  val = stat["PicardProjection"]["FieldEval1"]["OldIntegral"]
+  assert numpy.all(val == [0.0, 0.0, 100.0, 200.0, 300.0, 400.0, 500.0, 600.0, 700.0, 800.0, 900.0])
+
+def test_picard_fieldeval2_min_0():
+  val = stat["PicardProjection"]["FieldEval2"]["max"][0]
+  assert numpy.all(val == [0.0, 200.0, 400.0, 600.0, 800.0, 1000.0, 1200.0, 1400.0, 1600.0, 1800.0, 2000.0])
+
+def test_picard_fieldeval2_max_0():
+  val = stat["PicardProjection"]["FieldEval2"]["min"][0]
+  assert numpy.all(val == [0.0, 200.0, 400.0, 600.0, 800.0, 1000.0, 1200.0, 1400.0, 1600.0, 1800.0, 2000.0])
+
+def test_picard_fieldeval2_int_0():
+  val = stat["PicardProjection"]["FieldEval2"]["Integral0"]
+  assert numpy.all(val == [0.0, 200.0, 400.0, 600.0, 800.0, 1000.0, 1200.0, 1400.0, 1600.0, 1800.0, 2000.0])
+
+def test_picard_fieldeval2_oldint_0():
+  val = stat["PicardProjection"]["FieldEval2"]["OldIntegral0"]
+  assert numpy.all(val == [0.0, 0.0, 200.0, 400.0, 600.0, 800.0, 1000.0, 1200.0, 1400.0, 1600.0, 1800.0])
+
+def test_picard_fieldeval2_min_1():
+  val = stat["PicardProjection"]["FieldEval2"]["max"][1]
+  assert numpy.all(val == [0.0, 300.0, 600.0, 900.0, 1200.0, 1500.0, 1800.0, 2100.0, 2400.0, 2700.0, 3000.0])
+
+def test_picard_fieldeval2_max_1():
+  val = stat["PicardProjection"]["FieldEval2"]["min"][1]
+  assert numpy.all(val == [0.0, 300.0, 600.0, 900.0, 1200.0, 1500.0, 1800.0, 2100.0, 2400.0, 2700.0, 3000.0])
+
+def test_picard_fieldeval2_int_1():
+  val = stat["PicardProjection"]["FieldEval2"]["Integral1"]
+  assert numpy.all(val == [0.0, 300.0, 600.0, 900.0, 1200.0, 1500.0, 1800.0, 2100.0, 2400.0, 2700.0, 3000.0])
+
+def test_picard_fieldeval2_oldint_1():
+  val = stat["PicardProjection"]["FieldEval2"]["OldIntegral1"]
+  assert numpy.all(val == [0.0, 0.0, 300.0, 600.0, 900.0, 1200.0, 1500.0, 1800.0, 2100.0, 2400.0, 2700.0])
+
