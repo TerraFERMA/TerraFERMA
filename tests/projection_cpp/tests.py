7cdebb480b48940c443e64e1073239de4efdbd55
Cian Wilson <cwilson@ldeo.columbia.edu>
2011-10-25 23:46:36 -0400

    Updating to the latest version of buckettools, which includes an initial (undocumented) draft of cpp expressions.
    Schema changes in buckettools required an update of all test options files, including in this commit.
    
    Also, adding a simple test of cpp expressions (basic functionality, inter coefficient dependencies not working yet).

---
+++ tests/projection_cpp/tests.py
@@ -0,0 +1,198 @@
+
+from buckettools.statfile import parser
+import numpy
+
+stat = parser("projection.stat")
+
+def test_picard_field1_min():
+  val = stat["PicardProjection"]["Field1"]["min"]
+  assert numpy.all(val == [0.0, 100.0, 200.0, 300.0, 400.0, 500.0, 600.0, 700.0, 800.0, 900.0, 1000.0])
+
+def test_picard_field1_max():
+  val = stat["PicardProjection"]["Field1"]["max"]
+  assert numpy.all(val == [0.0, 100.0, 200.0, 300.0, 400.0, 500.0, 600.0, 700.0, 800.0, 900.0, 1000.0])
+
+def test_picard_field1_int():
+  val = stat["PicardProjection"]["Field1"]["Integral"]
+  assert numpy.all(val == [0.0, 100.0, 200.0, 300.0, 400.0, 500.0, 600.0, 700.0, 800.0, 900.0, 1000.0])
+
+def test_picard_source1_int():
+  val = stat["PicardProjection"]["Source1"]["Integral"]
+  assert numpy.all(val == [0.0, 100.0, 200.0, 300.0, 400.0, 500.0, 600.0, 700.0, 800.0, 900.0, 1000.0])
+
+def test_picard_pysource1_int():
+  val = stat["PicardProjection"]["PySource1"]["Integral"]
+  assert numpy.all(val == [0.0, 100.0, 200.0, 300.0, 400.0, 500.0, 600.0, 700.0, 800.0, 900.0, 1000.0])
+
+def test_picard_field1_oldint():
+  val = stat["PicardProjection"]["Field1"]["OldIntegral"]
+  assert numpy.all(val == [0.0, 0.0, 100.0, 200.0, 300.0, 400.0, 500.0, 600.0, 700.0, 800.0, 900.0])
+
+def test_picard_source1_oldint():
+  val = stat["PicardProjection"]["Source1"]["OldIntegral"]
+  assert numpy.all(val == [0.0, 0.0, 100.0, 200.0, 300.0, 400.0, 500.0, 600.0, 700.0, 800.0, 900.0])
+
+def test_picard_pysource1_oldint():
+  val = stat["PicardProjection"]["PySource1"]["OldIntegral"]
+  assert numpy.all(val == [0.0, 0.0, 100.0, 200.0, 300.0, 400.0, 500.0, 600.0, 700.0, 800.0, 900.0])
+
+def test_picard_field2_min_0():
+  val = stat["PicardProjection"]["Field2"]["max"][0]
+  assert numpy.all(val == [0.0, 200.0, 400.0, 600.0, 800.0, 1000.0, 1200.0, 1400.0, 1600.0, 1800.0, 2000.0])
+
+def test_picard_field2_max_0():
+  val = stat["PicardProjection"]["Field2"]["min"][0]
+  assert numpy.all(val == [0.0, 200.0, 400.0, 600.0, 800.0, 1000.0, 1200.0, 1400.0, 1600.0, 1800.0, 2000.0])
+
+def test_picard_field2_int_0():
+  val = stat["PicardProjection"]["Field2"]["Integral0"]
+  assert numpy.all(val == [0.0, 200.0, 400.0, 600.0, 800.0, 1000.0, 1200.0, 1400.0, 1600.0, 1800.0, 2000.0])
+
+def test_picard_source2_int_0():
+  val = stat["PicardProjection"]["Source2"]["Integral0"]
+  assert numpy.all(val == [0.0, 200.0, 400.0, 600.0, 800.0, 1000.0, 1200.0, 1400.0, 1600.0, 1800.0, 2000.0])
+
+def test_picard_pysource2_int_0():
+  val = stat["PicardProjection"]["PySource2"]["Integral0"]
+  assert numpy.all(val == [0.0, 200.0, 400.0, 600.0, 800.0, 1000.0, 1200.0, 1400.0, 1600.0, 1800.0, 2000.0])
+
+def test_picard_field2_oldint_0():
+  val = stat["PicardProjection"]["Field2"]["OldIntegral0"]
+  assert numpy.all(val == [0.0, 0.0, 200.0, 400.0, 600.0, 800.0, 1000.0, 1200.0, 1400.0, 1600.0, 1800.0])
+
+def test_picard_source2_oldint_0():
+  val = stat["PicardProjection"]["Source2"]["OldIntegral0"]
+  assert numpy.all(val == [0.0, 0.0, 200.0, 400.0, 600.0, 800.0, 1000.0, 1200.0, 1400.0, 1600.0, 1800.0])
+
+def test_picard_pysource2_oldint_0():
+  val = stat["PicardProjection"]["PySource2"]["OldIntegral0"]
+  assert numpy.all(val == [0.0, 0.0, 200.0, 400.0, 600.0, 800.0, 1000.0, 1200.0, 1400.0, 1600.0, 1800.0])
+
+def test_picard_field2_min_1():
+  val = stat["PicardProjection"]["Field2"]["max"][1]
+  assert numpy.all(val == [0.0, 300.0, 600.0, 900.0, 1200.0, 1500.0, 1800.0, 2100.0, 2400.0, 2700.0, 3000.0])
+
+def test_picard_field2_max_1():
+  val = stat["PicardProjection"]["Field2"]["min"][1]
+  assert numpy.all(val == [0.0, 300.0, 600.0, 900.0, 1200.0, 1500.0, 1800.0, 2100.0, 2400.0, 2700.0, 3000.0])
+
+def test_picard_field2_int_1():
+  val = stat["PicardProjection"]["Field2"]["Integral1"]
+  assert numpy.all(val == [0.0, 300.0, 600.0, 900.0, 1200.0, 1500.0, 1800.0, 2100.0, 2400.0, 2700.0, 3000.0])
+
+def test_picard_source2_int_1():
+  val = stat["PicardProjection"]["Source2"]["Integral1"]
+  assert numpy.all(val == [0.0, 300.0, 600.0, 900.0, 1200.0, 1500.0, 1800.0, 2100.0, 2400.0, 2700.0, 3000.0])
+
+def test_picard_pysource2_int_1():
+  val = stat["PicardProjection"]["PySource2"]["Integral1"]
+  assert numpy.all(val == [0.0, 300.0, 600.0, 900.0, 1200.0, 1500.0, 1800.0, 2100.0, 2400.0, 2700.0, 3000.0])
+
+def test_picard_field2_oldint_1():
+  val = stat["PicardProjection"]["Field2"]["OldIntegral1"]
+  assert numpy.all(val == [0.0, 0.0, 300.0, 600.0, 900.0, 1200.0, 1500.0, 1800.0, 2100.0, 2400.0, 2700.0])
+
+def test_picard_source2_oldint_1():
+  val = stat["PicardProjection"]["Source2"]["OldIntegral1"]
+  assert numpy.all(val == [0.0, 0.0, 300.0, 600.0, 900.0, 1200.0, 1500.0, 1800.0, 2100.0, 2400.0, 2700.0])
+
+def test_picard_pysource2_oldint_1():
+  val = stat["PicardProjection"]["PySource2"]["OldIntegral1"]
+  assert numpy.all(val == [0.0, 0.0, 300.0, 600.0, 900.0, 1200.0, 1500.0, 1800.0, 2100.0, 2400.0, 2700.0])
+
+def test_snes_field1_min():
+  val = stat["SNESProjection"]["Field1"]["min"]
+  assert numpy.all(val == [0.0, 100.0, 200.0, 300.0, 400.0, 500.0, 600.0, 700.0, 800.0, 900.0, 1000.0])
+
+def test_snes_field1_max():
+  val = stat["SNESProjection"]["Field1"]["max"]
+  assert numpy.all(val == [0.0, 100.0, 200.0, 300.0, 400.0, 500.0, 600.0, 700.0, 800.0, 900.0, 1000.0])
+
+def test_snes_field1_int():
+  val = stat["SNESProjection"]["Field1"]["Integral"]
+  assert numpy.all(val == [0.0, 100.0, 200.0, 300.0, 400.0, 500.0, 600.0, 700.0, 800.0, 900.0, 1000.0])
+
+def test_snes_source1_int():
+  val = stat["SNESProjection"]["Source1"]["Integral"]
+  assert numpy.all(val == [0.0, 100.0, 200.0, 300.0, 400.0, 500.0, 600.0, 700.0, 800.0, 900.0, 1000.0])
+
+def test_snes_pysource1_int():
+  val = stat["SNESProjection"]["PySource1"]["Integral"]
+  assert numpy.all(val == [0.0, 100.0, 200.0, 300.0, 400.0, 500.0, 600.0, 700.0, 800.0, 900.0, 1000.0])
+
+def test_snes_field1_oldint():
+  val = stat["SNESProjection"]["Field1"]["OldIntegral"]
+  assert numpy.all(val == [0.0, 0.0, 100.0, 200.0, 300.0, 400.0, 500.0, 600.0, 700.0, 800.0, 900.0])
+
+def test_snes_source1_oldint():
+  val = stat["SNESProjection"]["Source1"]["OldIntegral"]
+  assert numpy.all(val == [0.0, 0.0, 100.0, 200.0, 300.0, 400.0, 500.0, 600.0, 700.0, 800.0, 900.0])
+
+def test_snes_pysource1_oldint():
+  val = stat["SNESProjection"]["PySource1"]["OldIntegral"]
+  assert numpy.all(val == [0.0, 0.0, 100.0, 200.0, 300.0, 400.0, 500.0, 600.0, 700.0, 800.0, 900.0])
+
+def test_snes_field2_min_0():
+  val = stat["SNESProjection"]["Field2"]["max"][0]
+  assert numpy.all(val == [0.0, 200.0, 400.0, 600.0, 800.0, 1000.0, 1200.0, 1400.0, 1600.0, 1800.0, 2000.0])
+
+def test_snes_field2_max_0():
+  val = stat["SNESProjection"]["Field2"]["min"][0]
+  assert numpy.all(val == [0.0, 200.0, 400.0, 600.0, 800.0, 1000.0, 1200.0, 1400.0, 1600.0, 1800.0, 2000.0])
+
+def test_snes_field2_int_0():
+  val = stat["SNESProjection"]["Field2"]["Integral0"]
+  assert numpy.all(val == [0.0, 200.0, 400.0, 600.0, 800.0, 1000.0, 1200.0, 1400.0, 1600.0, 1800.0, 2000.0])
+
+def test_snes_source2_int_0():
+  val = stat["SNESProjection"]["Source2"]["Integral0"]
+  assert numpy.all(val == [0.0, 200.0, 400.0, 600.0, 800.0, 1000.0, 1200.0, 1400.0, 1600.0, 1800.0, 2000.0])
+
+def test_snes_pysource2_int_0():
+  val = stat["SNESProjection"]["PySource2"]["Integral0"]
+  assert numpy.all(val == [0.0, 200.0, 400.0, 600.0, 800.0, 1000.0, 1200.0, 1400.0, 1600.0, 1800.0, 2000.0])
+
+def test_snes_field2_oldint_0():
+  val = stat["SNESProjection"]["Field2"]["OldIntegral0"]
+  assert numpy.all(val == [0.0, 0.0, 200.0, 400.0, 600.0, 800.0, 1000.0, 1200.0, 1400.0, 1600.0, 1800.0])
+
+def test_snes_source2_oldint_0():
+  val = stat["SNESProjection"]["Source2"]["OldIntegral0"]
+  assert numpy.all(val == [0.0, 0.0, 200.0, 400.0, 600.0, 800.0, 1000.0, 1200.0, 1400.0, 1600.0, 1800.0])
+
+def test_snes_pysource2_oldint_0():
+  val = stat["SNESProjection"]["PySource2"]["OldIntegral0"]
+  assert numpy.all(val == [0.0, 0.0, 200.0, 400.0, 600.0, 800.0, 1000.0, 1200.0, 1400.0, 1600.0, 1800.0])
+
+def test_snes_field2_min_1():
+  val = stat["SNESProjection"]["Field2"]["max"][1]
+  assert numpy.all(val == [0.0, 300.0, 600.0, 900.0, 1200.0, 1500.0, 1800.0, 2100.0, 2400.0, 2700.0, 3000.0])
+
+def test_snes_field2_max_1():
+  val = stat["SNESProjection"]["Field2"]["min"][1]
+  assert numpy.all(val == [0.0, 300.0, 600.0, 900.0, 1200.0, 1500.0, 1800.0, 2100.0, 2400.0, 2700.0, 3000.0])
+
+def test_snes_field2_int_1():
+  val = stat["SNESProjection"]["Field2"]["Integral1"]
+  assert numpy.all(val == [0.0, 300.0, 600.0, 900.0, 1200.0, 1500.0, 1800.0, 2100.0, 2400.0, 2700.0, 3000.0])
+
+def test_snes_source2_int_1():
+  val = stat["SNESProjection"]["Source2"]["Integral1"]
+  assert numpy.all(val == [0.0, 300.0, 600.0, 900.0, 1200.0, 1500.0, 1800.0, 2100.0, 2400.0, 2700.0, 3000.0])
+
+def test_snes_pysource2_int_1():
+  val = stat["SNESProjection"]["PySource2"]["Integral1"]
+  assert numpy.all(val == [0.0, 300.0, 600.0, 900.0, 1200.0, 1500.0, 1800.0, 2100.0, 2400.0, 2700.0, 3000.0])
+
+def test_snes_field2_oldint_1():
+  val = stat["SNESProjection"]["Field2"]["OldIntegral1"]
+  assert numpy.all(val == [0.0, 0.0, 300.0, 600.0, 900.0, 1200.0, 1500.0, 1800.0, 2100.0, 2400.0, 2700.0])
+
+def test_snes_source2_oldint_1():
+  val = stat["SNESProjection"]["Source2"]["OldIntegral1"]
+  assert numpy.all(val == [0.0, 0.0, 300.0, 600.0, 900.0, 1200.0, 1500.0, 1800.0, 2100.0, 2400.0, 2700.0])
+
+def test_snes_pysource2_oldint_1():
+  val = stat["SNESProjection"]["PySource2"]["OldIntegral1"]
+  assert numpy.all(val == [0.0, 0.0, 300.0, 600.0, 900.0, 1200.0, 1500.0, 1800.0, 2100.0, 2400.0, 2700.0])
+
