41643a8c204b15850ad2119e8d0aeba192ca35c3
Marc Spiegelman <mspieg@ldeo.columbia.edu>
2011-11-22 16:45:11 -0500

    made subductionbenchmark1c tests more verbose

--- tests/subductionbenchmark1c_split/tests.py
+++ tests/subductionbenchmark1c_split/tests.py
@@ -7,13 +7,19 @@ det = parser("subduction.det")
 
 def test_T_11_11():
   val = det["Temperature"]["Temperature"]["SlabPoint"][0,-1]-273.
+  test=388.5
   assert abs(val - 388.5) < 1.0
+  print "\tvalue=", val, "test=", test, " ",
 
 def test_T_slab():
   val = sqrt(sum((det["Temperature"]["Temperature"]["Slab"][:,-1]-273.)**2)/36.)
-  assert abs(val - 504.0) < 1.0
+  test= 504.0
+  assert abs(val - test) < 1.0
+  print "\tvalue=", val, "test=", test, " ",
 
 def test_T_wedge():
   val = sqrt(sum((det["Temperature"]["Temperature"]["Wedge"][:,-1]-273.)**2)/78.)
+  test=853.5
   assert abs(val - 853.5) < 1.0
+  print "\tvalue=", val, "test=", test, " ",
 
