72145f0316217a54c8afb113f89d60149cb27399
Cian Wilson <cwilson@ldeo.columbia.edu>
2012-12-13 19:56:22 -0500

    Fix typo

--- buckettools/cpp/SolverBucket.cpp
+++ buckettools/cpp/SolverBucket.cpp
@@ -741,7 +741,7 @@ void SolverBucket::ksp_check_convergence_(KSP &ksp, int indent)
                               indentation.c_str(), kspreason);
   dolfin::log(dolfin::INFO, "%sKSP n/o iterations %d", 
                               indentation.c_str(), kspiterations);
-  if (ident==0 && kspreason<0)
+  if (indent==0 && kspreason<0)
   {
     if (ignore_failures_)
     {
