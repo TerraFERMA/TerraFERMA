68b9fdbaea525df701a92b4f201b0e9c43f830ed
Cian Wilson <cwilson@carnegiescience.edu>
2022-07-26 01:09:07 -0400

    Use apt's openblas instead.
    And try to speed up builds by using the cache.

--- docker/build.sh
+++ docker/build.sh
@@ -3,19 +3,19 @@
 
 # base
 
-docker buildx build --no-cache --file docker/focal/base/Dockerfile --platform linux/arm64             --tag terraferma/base:fenics-2019.1.0-focal-arm64 --push .
-docker buildx build --no-cache --file docker/focal/base/Dockerfile --platform linux/amd64             --tag terraferma/base:fenics-2019.1.0-focal-amd64 --push .
-docker buildx build --no-cache --file docker/focal/base/Dockerfile --platform linux/amd64,linux/arm64 --tag terraferma/base:fenics-2019.1.0-focal       --push .
+docker buildx build --file docker/focal/base/Dockerfile --platform linux/arm64             --tag terraferma/base:fenics-2019.1.0-focal-arm64 --push .
+docker buildx build --file docker/focal/base/Dockerfile --platform linux/amd64             --tag terraferma/base:fenics-2019.1.0-focal-amd64 --push .
+docker buildx build --file docker/focal/base/Dockerfile --platform linux/amd64,linux/arm64 --tag terraferma/base:fenics-2019.1.0-focal       --push .
 
 # dev-env
 
-docker buildx build --no-cache --build-arg TAG=fenics-2019.1.0-focal-arm64 --file docker/focal/dev-env/Dockerfile --platform linux/arm64             --tag terraferma/dev-env:fenics-2019.1.0-focal-arm64 --push .
-docker buildx build --no-cache --build-arg TAG=fenics-2019.1.0-focal-amd64 --file docker/focal/dev-env/Dockerfile --platform linux/amd64             --tag terraferma/dev-env:fenics-2019.1.0-focal-amd64 --push .
-docker buildx build --no-cache --build-arg TAG=fenics-2019.1.0-focal       --file docker/focal/dev-env/Dockerfile --platform linux/amd64,linux/arm64 --tag terraferma/dev-env:fenics-2019.1.0-focal       --push .
+docker buildx build --build-arg TAG=fenics-2019.1.0-focal-arm64 --file docker/focal/dev-env/Dockerfile --platform linux/arm64             --tag terraferma/dev-env:fenics-2019.1.0-focal-arm64 --push .
+docker buildx build --build-arg TAG=fenics-2019.1.0-focal-amd64 --file docker/focal/dev-env/Dockerfile --platform linux/amd64             --tag terraferma/dev-env:fenics-2019.1.0-focal-amd64 --push .
+docker buildx build --build-arg TAG=fenics-2019.1.0-focal       --file docker/focal/dev-env/Dockerfile --platform linux/amd64,linux/arm64 --tag terraferma/dev-env:fenics-2019.1.0-focal       --push .
 
 # dev
 
-docker buildx build --no-cache --build-arg TAG=fenics-2019.1.0-focal-arm64 --file docker/focal/dev/Dockerfile --platform linux/arm64             --tag terraferma/dev:fenics-2019.1.0-focal-arm64 --push .
-docker buildx build --no-cache --build-arg TAG=fenics-2019.1.0-focal-amd64 --file docker/focal/dev/Dockerfile --platform linux/amd64             --tag terraferma/dev:fenics-2019.1.0-focal-amd64 --push .
-docker buildx build --no-cache --build-arg TAG=fenics-2019.1.0-focal       --file docker/focal/dev/Dockerfile --platform linux/amd64,linux/arm64 --tag terraferma/dev:fenics-2019.1.0-focal       --push .
+docker buildx build --build-arg TAG=fenics-2019.1.0-focal-arm64 --file docker/focal/dev/Dockerfile --platform linux/arm64             --tag terraferma/dev:fenics-2019.1.0-focal-arm64 --push .
+docker buildx build --build-arg TAG=fenics-2019.1.0-focal-amd64 --file docker/focal/dev/Dockerfile --platform linux/amd64             --tag terraferma/dev:fenics-2019.1.0-focal-amd64 --push .
+docker buildx build --build-arg TAG=fenics-2019.1.0-focal       --file docker/focal/dev/Dockerfile --platform linux/amd64,linux/arm64 --tag terraferma/dev:fenics-2019.1.0-focal       --push .
 
